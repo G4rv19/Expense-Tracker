5044e00efe500dd29773c10e378745d1e30f80d2
G4rv19 <123931565+G4rv19@users.noreply.github.com>
2024-01-12 17:54:13 +0000

    Basic structure of the expense tracker.

---
+++ Expense-tracker.py
@@ -0,0 +1,23 @@
+#create a place to store the input of expenses.
+
+
+#get the input from user in 2 ways.
+# ask user for category first, if existing then make that category priority.
+#firstly ask for what is purchases.
+#secondly ask for what is the price.
+
+
+
+"""
+example of the storing data of input under food category.
+
+expenses = [
+food
+(pizza),($12)
+(ice cream),($10)
+
+rent
+(rent),($120)
+]
+"""
+#when printed into csv make the category bold and remove the "()" sign and make it look good. otherwise keep it like this and import into sql database.
